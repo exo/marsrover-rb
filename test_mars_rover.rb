a7e0e95f933f234e45ed429ec2c623ee0ffd6c28
Jon Simpson <me@jonsimpson.co.uk>
2013-07-22 13:13:04 +0100

    Add failing test for backward command

--- test_mars_rover.rb
+++ test_mars_rover.rb
@@ -10,4 +10,11 @@ class TestMarsRover < Test::Unit::TestCase
     assert_equal([1,0,'N'], rover.position())
   end
 
+  # Test basic function of the backward command.
+  def test_backward
+    rover = MarsRover.new(1,0,'N')
+    rover.command('B')
+    assert_equal([0,0,'N'], rover.position())
+  end
+
 end
