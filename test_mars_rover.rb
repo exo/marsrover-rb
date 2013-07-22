5d136f220f45b19b2fb0755d83e69e40fb4e174e
Jon Simpson <me@jonsimpson.co.uk>
2013-07-22 14:01:43 +0100

    Modify tests, forward is y+1, backward y-1.

--- test_mars_rover.rb
+++ test_mars_rover.rb
@@ -7,12 +7,12 @@ class TestMarsRover < Test::Unit::TestCase
   def test_forward
     rover = MarsRover.new
     rover.command('F')
-    assert_equal([1,0,'N'], rover.position)
+    assert_equal([0,1,'N'], rover.position)
   end
 
   # Test basic function of the backward command.
   def test_backward
-    rover = MarsRover.new(1,0,'N')
+    rover = MarsRover.new(0,1,'N')
     rover.command('B')
     assert_equal([0,0,'N'], rover.position)
   end
