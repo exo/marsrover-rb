a490959ec8aee251c2ec1293ea24b79adf621615
Jon Simpson <me@jonsimpson.co.uk>
2013-07-22 13:09:19 +0100

    Pass forward test, basic rover outline.

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,2 +1,26 @@
 class MarsRover
+
+  def initialize (x, y, heading)
+    @x, @y = x, y
+    @heading = heading
+  end
+
+  def position
+    return [@x, @y, @heading]
+  end
+
+  def command (command_string)
+    case command_string
+    when 'F'
+      forward
+    end
+  end
+
+  def forward
+    case @heading
+    when 'N'
+      @x, @y = (@x + 1), @y
+    end
+  end
+
 end
