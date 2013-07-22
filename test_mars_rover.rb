3221acf480fb0ac0dc71ba91b4e1c8e3244b808b
Jon Simpson <me@jonsimpson.co.uk>
2013-07-22 13:03:53 +0100

    Add forward command test.

--- test_mars_rover.rb
+++ test_mars_rover.rb
@@ -2,4 +2,12 @@ require "./mars_rover"
 require "test/unit"
 
 class TestMarsRover < Test::Unit::TestCase
+
+  # Tests basic function of the forward command.
+  def test_forward
+    rover = MarsRover.new(0, 0, 'N')
+    rover.command('F')
+    assert_equal([1,0,'N'], rover.position())
+  end
+
 end
