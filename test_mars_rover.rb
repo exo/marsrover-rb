c3963a0f44c0d5577b90a817ee4a6c2c2cdf1160
Jon Simpson <me@jonsimpson.co.uk>
2014-01-27 10:57:02 +0000

    Tests updated to expect additional compass points

--- test_mars_rover.rb
+++ test_mars_rover.rb
@@ -26,14 +26,14 @@ class TestMarsRover < Test::Unit::TestCase
   def test_left_turn
     rover = MarsRover.new
     rover.command('L')
-    assert_equal([0,0,'W'], rover.position)
+    assert_equal([0,0,'NW'], rover.position)
   end
 
   # Test right turn
   def test_right_turn
     rover = MarsRover.new
     rover.command('R')
-    assert_equal([0,0,'E'], rover.position)
+    assert_equal([0,0,'NE'], rover.position)
   end
 
   # Test multiple commands
@@ -54,21 +54,21 @@ class TestMarsRover < Test::Unit::TestCase
   def test_left_backward
     rover = MarsRover.new
     rover.command('LB')
-    assert_equal([1,0,'W'], rover.position)
+    assert_equal([1,0,'NW'], rover.position)
   end
 
   # Test multiple turns for a complete rotation
   def test_complete_rotation
     rover = MarsRover.new
-    rover.command('LLLL')
+    rover.command('LLLLLLLL')
     assert_equal([0,0,'N'], rover.position)
   end
 
-  # Kata example test - 'FFRFF' puts the rover at [2,2,'E'].
+  # Kata example test - 'FFRFF' puts the rover at [0,0,'NE'].
   def test_ffrff
     rover = MarsRover.new
     rover.command('FFRFF')
-    assert_equal([2,2, 'E'], rover.position)
+    assert_equal([0,0, 'NE'], rover.position)
   end
 
   # Test world class exists and a default world should be 5x5
@@ -79,18 +79,18 @@ class TestMarsRover < Test::Unit::TestCase
   end
 
   # Test left edge wrapping
-  # when the robot goes left at x=0, it should be at x=4 in a 5x5 world.
+  # when the robot goes west at x=0, it should be at x=4 in a 5x5 world.
   def test_left_wrap
-    rover = MarsRover.new(0,0,'N', world=World.new(5,5))
-    rover.command('LF')
+    rover = MarsRover.new(0,0,'W', world=World.new(5,5))
+    rover.command('LLF')
     assert_equal([4,0,'W'], rover.position)
   end
 
   # Test right edge wrapping
-  # when the robot goes right of x=4, it should be at x=0 in a 5x5 world
+  # when the robot goes east of x=4, it should be at x=0 in a 5x5 world
   def test_right_wrap
-    rover = MarsRover.new(4,0,'N', world=World.new(5,5))
-    rover.command('RF')
+    rover = MarsRover.new(4,0,'E', world=World.new(5,5))
+    rover.command('F')
     assert_equal([0,0,'E'], rover.position)
   end
 
@@ -154,11 +154,11 @@ class TestMarsRover < Test::Unit::TestCase
     rover = MarsRover.new(0,0,'E', world)
     message = rover.command('F')
     assert_equal("Obstacle found at x:1 y:0", message)
-    message = rover.command('LFRF')
+    message = rover.command('LLFRRF')
     assert_equal("Obstacle found at x:1 y:1", message)
-    message = rover.command('LFRF')
+    message = rover.command('LLFRRF')
     assert_equal("Obstacle found at x:1 y:2", message)
-    message = rover.command('LFRF')
+    message = rover.command('LLFRRF')
     assert_equal([1,3,'E'], rover.position)
   end
 end
